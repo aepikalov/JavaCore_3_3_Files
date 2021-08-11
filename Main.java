54e70be05f7d3bee26edc10e9eb182feabc580f2
Artem Pikalov <aepikalov@gmail.com>
2021-08-12 01:34:48 +0400

    File Task 3 v1

---
+++ Main.java
@@ -0,0 +1,47 @@
+package JavaCore_3_3_Files;
+
+import JavaCore_3_2_Files.GameProgress;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        openZip("D://Games//savegames//zip.zip");
+        System.out.println(openProgress("D://Games//savegames//game1.dat"));
+    }
+
+    private static void openZip(String pathZip) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(pathZip))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static GameProgress openProgress(String path) {
+        GameProgress gameProgress = null;
+        try (FileInputStream fis = new FileInputStream(path);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return gameProgress;
+    }
+}
